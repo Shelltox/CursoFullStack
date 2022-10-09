ec773600b509b6db32511abdb675b73395cb5180
Martin Rodriguez <martinrod314@gmail.com>
2022-10-09 19:06:45 -0300

    tarea

--- RadioVia/backend/index.js
+++ RadioVia/backend/index.js
@@ -1,3 +1,15 @@
+
+var express = require('express');
+var app = express();
+const moment = require('moment');
+console.log('Naci ' + moment('02/09/1980','dd/mm/yyyy').fromNow())
+
+/*
+var express = require('express');
+var app = express();
+const moment = require('moment');
+console.log('Naci ' + moment('02/09/1980','dd/mm/yyyy').fromNow())
+
 var hoy = new Date();
 console.log('Hoy es ' + hoy);
 
@@ -5,3 +17,5 @@ var i;
 for (i=0;i<10;i++){
     console.log(i);
 }
+
+*/
